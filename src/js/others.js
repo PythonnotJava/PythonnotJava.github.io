c6eefbb6d03bcf98b881655293c7315090812e96
PythonnotJava <2565497078@qq.com>
2024-10-05 19:02:42 +0800

    NEW

--- src/js/others.js
+++ src/js/others.js
@@ -40,7 +40,7 @@
         title: 'C#入门笔记',
         content: 'C#入门笔记，基于.net 8.0',
         icon: 'https://th.bing.com/th?id=OSK.GhOfIXkD4m2VC8BFLppZ1d0-SeI8Fj5pLmObRQzFIyY&w=102&h=102&c=7&o=6&dpr=1.3&pid=SANGAM',
-        link: '../../h5/other/C#入门笔记.html'
+        link: '../../h5/other/C-sharp入门笔记.html'
       },
     ];
 
